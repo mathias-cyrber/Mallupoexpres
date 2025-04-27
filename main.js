2f3972e2ebf47b7c94f86dbb00c207150d868bff
Trita store.py <raskevichusmathias@gmail.com>
2025-04-26 21:03:48 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,24 +1,26 @@
 const searchBar = document.querySelector('.search-bar');
 
 searchBar.addEventListener('keypress', function(event) {
-  if (event.key === 'Enter') { // Detectar la tecla "Enter"
-    const searchTerm = searchBar.value.toLowerCase(); // Obtener el texto y convertirlo a minúsculas
+  if (event.key === 'Enter') {
+    const searchTerm = searchBar.value.toLowerCase();
 
     if (searchTerm === 'gamer') {
       window.location.href = '#gamer';
     } else if (searchTerm === 'muebles') {
       window.location.href = '#muebles';
+    } else if (searchTerm === 'televisiones') {  // Añadido
+      window.location.href = '#televisiones';
     } else if (searchTerm === 'contacto') {
       window.location.href = '#contacto';
     } else {
-      alert('No se encontró la sección.'); // Opcional: mostrar un mensaje si no hay coincidencia
+      alert('No se encontró la sección.');
     }
   }
 });
 
-const navToggle = document.querySelector('.nav-toggle');  // Botón
-const navLinks = document.querySelector('.nav-links');    // Menú
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelector('.nav-links');
 
 navToggle.addEventListener('click', function() {
-    navLinks.classList.toggle('active');  // Alternar la clase 'active'
+    navLinks.classList.toggle('active');
 });
